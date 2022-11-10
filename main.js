6c472789a8aa08a1cc89d009705d85a73435f263
Andres Felipe OQ <105876623+andresfelipeoq@users.noreply.github.com>
2022-11-10 10:25:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+/* const altura = document.body.scrollHeight - window.innerHeight;
+const fondo = document.getElementById('fondo');
+
+window.onscroll = () => {
+    const anchoFondo = (window.pageYOffset / altura) * 900;
+    fondo.style.width = anchofondo + '%'; 
+} */
+
+function saludo (){
+    alert ("hola calabaza");
+}
+
